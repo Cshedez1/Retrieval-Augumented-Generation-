e7d4783df0b3ff5b821e5704208e2c0f12320f34
Cshedez1 <161688060+Cshedez1@users.noreply.github.com>
2024-04-25 18:50:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,13 @@ def generate_response(uploaded_file, openai_api_key, query_text):
    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
    texts = text_splitter.create_documents(documents)
    # Select embeddings
-   embeddings = OpenAIEmbeddings(openai_api_key="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")
+   embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
    # Create a vectorstore from documents
    db = Chroma.from_documents(texts, embeddings)
    # Create retriever interface
    retriever = db.as_retriever()
    # Create QA chain
-   qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key="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"),
+   qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key),
 chain_type='stuff', retriever=retriever)
    return qa.run(query_text)
 
